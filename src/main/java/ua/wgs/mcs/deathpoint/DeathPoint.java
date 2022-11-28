3a3ec007b6f7d1dfff38b37824d24c51cbdd4160
Nikita Chekun <chekun2004@gmail.com>
2022-11-28 21:30:00 +0200

    Project init.

---
+++ src/main/java/ua/wgs/mcs/deathpoint/DeathPoint.java
@@ -0,0 +1,17 @@
+package ua.wgs.mcs.deathpoint;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DeathPoint extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
