f1700535cd44ba30aef6d30987c8b23935351b7c
Nikita Chekun <chekun2004@gmail.com>
2022-11-28 22:26:02 +0200

    Add player death message.

--- src/main/java/ua/wgs/mcs/deathpoint/DeathPoint.java
+++ src/main/java/ua/wgs/mcs/deathpoint/DeathPoint.java
@@ -1,8 +1,27 @@
 package ua.wgs.mcs.deathpoint;
 
+import org.bukkit.Server;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import ua.wgs.mcs.deathpoint.events.MainEventHandler;
 
 @SuppressWarnings("unused")
 public final class DeathPoint extends JavaPlugin {
 
+    private static DeathPoint plugin;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+        Server server = getServer();
+        PluginManager pluginManager = server.getPluginManager();
+
+        saveDefaultConfig();
+
+        pluginManager.registerEvents(new MainEventHandler(), this);
+    }
+
+    public static DeathPoint getPlugin() {
+        return plugin;
+    }
 }
