0fe0ebd10e616cc7c1259978a32f61821318fcad
Nikita Chekun <chekun2004@gmail.com>
2022-12-11 12:59:55 +0200

    Add dimension to message.

--- src/main/java/ua/wgs/mcs/deathpoint/events/OnPlayerDeathEventHandler.java
+++ src/main/java/ua/wgs/mcs/deathpoint/events/OnPlayerDeathEventHandler.java
@@ -1,5 +1,6 @@
 package ua.wgs.mcs.deathpoint.events;
 
+import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.event.HoverEvent;
@@ -7,6 +8,7 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.Location;
 import org.bukkit.Server;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,6 +23,7 @@ public final class OnPlayerDeathEventHandler implements Listener {
         Player player = e.getPlayer();
         Server server = player.getServer();
         Location playerLocation = player.getLocation();
+        World playerWorld = playerLocation.getWorld();
         TranslationConfig translation = DeathPoint.translation();
 
         String broadcastTemplate = translation.getString("PlayerDeathAtLocation.broadcast");
@@ -28,13 +31,16 @@ public final class OnPlayerDeathEventHandler implements Listener {
         String locationTemplate = translation.getString("PlayerDeathAtLocation.components.location.template");
         String locationHoverTemplate = translation.getString("PlayerDeathAtLocation.components.location.hover");
         String locationSuggestTemplate = translation.getString("PlayerDeathAtLocation.components.location.suggest");
+        String worldTemplate = translation.getString("PlayerDeathAtLocation.components.world." +
+                nameOfEnvironment(playerWorld.getEnvironment()));
 
         String playerSuggest = playerSuggestTemplate.replace("<name>", player.getName());
 
         String locationSuggest = locationSuggestTemplate
                 .replace("<x>", String.valueOf((int) playerLocation.getX()))
                 .replace("<y>", String.valueOf((int) playerLocation.getY()))
-                .replace("<z>", String.valueOf((int) playerLocation.getZ()));
+                .replace("<z>", String.valueOf((int) playerLocation.getZ()))
+                .replace("<world>", keyOfEnvironment(playerWorld.getEnvironment()));
 
         Component playerLocationComponent = MiniMessage.miniMessage().deserialize(locationTemplate,
                 Placeholder.parsed("x", String.valueOf((int) playerLocation.getX())),
@@ -43,16 +49,38 @@ public final class OnPlayerDeathEventHandler implements Listener {
 
         Component locationHoverComponent = MiniMessage.miniMessage().deserialize(locationHoverTemplate);
 
+        Component worldComponent = MiniMessage.miniMessage().deserialize(worldTemplate);
+
         Component messageComponent = MiniMessage.miniMessage().deserialize(broadcastTemplate,
                 Placeholder.component("player", Component
                         .text(player.getName())
-                        .hoverEvent(HoverEvent.showText(Component.text(player.getName() + "\n" + player.getUniqueId())))
+                        .hoverEvent(HoverEvent.showEntity(Key.key("minecraft:player"),
+                                player.getUniqueId(), Component.text(player.getName())))
                         .clickEvent(ClickEvent.suggestCommand(playerSuggest))),
                 Placeholder.component("location", playerLocationComponent
                         .hoverEvent(HoverEvent.showText(locationHoverComponent))
-                        .clickEvent(ClickEvent.suggestCommand(locationSuggest))));
+                        .clickEvent(ClickEvent.suggestCommand(locationSuggest))),
+                Placeholder.component("world", worldComponent));
 
         server.broadcast(messageComponent);
+    }
+
+    private String nameOfEnvironment(World.Environment environment) {
+        return switch (environment) {
+            case NORMAL -> "overworld";
+            case NETHER -> "the_nether";
+            case THE_END -> "the_end";
+            default -> environment.name();
+        };
+    }
+
+    private String keyOfEnvironment(World.Environment environment) {
+        return switch (environment) {
+            case NORMAL -> "minecraft:overworld";
+            case NETHER -> "minecraft:the_nether";
+            case THE_END -> "minecraft:the_end";
+            default -> environment.name();
+        };
     }
 }
 
